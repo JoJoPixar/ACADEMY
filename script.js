026fab1e589dbfb72a543a6d632f0aea61ba5fce
Andrey Trofimov <nagunius11@gmail.com>
2020-06-05 15:12:36 +0700

    Сделал второе задание

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+    let money = 50000;
+    let income = 'пассивчик';
+    let addExpenses = 'комуналОчка, связь, интернет, дошики';
+    let deposit = 1 > 0;
+    let mission = 1000000;
+    let period = 12;
 
-alert('hello there');
-console.log('still there?');
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log(addExpenses.length);
+    console.log('Период равен ' + period + ' месяцев и цель заработать ' + mission + ' рублей');
+addExpenses.split();
+console.log(addExpenses.toLowerCase());
+let budgetDay = money/30;
+console.log(budgetDay);
